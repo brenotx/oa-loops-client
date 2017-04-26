d1a6feefac6d2689f053e00a44900e5f9e924bbe
BrenoTeixeira <brenotx@gmail.com>
2017-04-26 08:00:18 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+// import './App.css';
+import React, { Component } from 'react';
+import Game from './components/Game';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        // Variable used to identify the game user instance
+        this.state = { gameId: 1 };
+    }
+    /**
+    * Create a new gameId for each user
+    */
+    createNewGame() {
+        this.setState({ gameId: this.state.gameId + 1 });
+    }
+
+    render() {
+        return (
+            <div>
+                <Game key={this.state.gameId}
+                      createNewGame={this.createNewGame.bind(this)}
+                      rows={5} columns={5} activeCellsCount={6} />
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Game.js
@@ -0,0 +1,100 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+import Row from './Row';
+import Cell from './Cell';
+import Footer from './Footer';
+
+class Game extends Component {
+    constructor(props) {
+        super(props);
+
+        this.matrix = [];
+        for (let r = 0; r < this.props.rows; r++) {
+            let row = [];
+            for (let c = 0; c < this.props.columns; c++) {
+                row.push(`${r}${c}`);
+            }
+            this.matrix.push(row);
+        }
+
+        let flatMatrix = _.flatten(this.matrix);
+        // this.activeCells = _.sampleSize(flatMatrix, this.props.activeCellsCount);
+        this.activeCells = ["20", "21", "22", "23", "13", "14"];
+
+
+        this.state = {
+            gameState: 'ready',
+            wrongGuesses: [],
+            correctGuesses: []
+        };
+    }
+    componentDidMount() {
+        this.memorizeTimerId = setTimeout(() => {
+            this.setState({ gameState: 'memorize' }, () => {
+                this.recallTimerId = setTimeout(this.startRecallMode.bind(this), 2000);
+            });
+        }, 2000);
+    }
+    componentWillUnmount() {
+        clearTimeout(this.memorizeTimerId);
+        clearTimeout(this.recallTimerId);
+        this.finishGame();
+    }
+    startRecallMode() {
+        this.setState({ gameState: 'recall' }, () => {
+            this.secondsRemaining = this.props.timeoutSeconds;
+            setInterval(() => {
+                 if (--this.secondsRemaining === 0) {
+                     this.setState({ gameState: this.finishGame("lost") });
+                 }
+             }, 1000);
+         });
+    }
+    finishGame(gameState) {
+        clearInterval(this.playTimerId);
+        return gameState;
+    }
+    recordGuess({ cellId, userGuessIsCorrect}) {
+        let { wrongGuesses, correctGuesses, gameState } = this.state;
+        if (userGuessIsCorrect) {
+            correctGuesses.push(cellId);
+            if (correctGuesses.length === this.props.activeCellsCount) {
+                gameState = this.finishGame("won");
+            }
+        } else {
+            wrongGuesses.push(cellId);
+            if (wrongGuesses.length > this.props.allowedWrongAttempts) {
+                gameState = this.finishGame("lost");
+            }
+        }
+        this.setState({ wrongGuesses, correctGuesses , gameState })
+    }
+    render() {
+        let showActiveCells = ["memorize", "lost"].indexOf(this.state.gameState) >= 0;
+        return (
+            <div className="grid">
+                {this.matrix.map((row, idx) => (
+                    <Row key={idx}>
+                        {row.map(cellId => <Cell key={cellId} id={cellId}
+                                                 showActiveCells={showActiveCells}
+                                                 activeCells={this.activeCells}
+                                                 recordGuess={this.recordGuess.bind(this)}
+                                                 {...this.state} />)}
+                    </Row>
+                ))}
+                <Footer {...this.state}
+                        playAgain={this.props.createNewGame}
+                        activeCellsCount={this.props.activeCellsCount} />
+            </div>
+        );
+    }
+}
+
+// Game configs
+Game.defaultProps = {
+    allowedWrongAttempts: 2,
+    timeoutSeconds: 10
+};
+
+export default Game;
