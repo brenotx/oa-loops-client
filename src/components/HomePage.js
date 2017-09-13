dafa9b230ea55383d9f2469daf9d6430a1e34b00
Breno Teixeira <brenotx@gmail.com>
2017-09-13 14:29:04 -0300

    Adding a better home page text

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Codex from './Codex';
+import Codex from "./Codex";
 
 class HomePage extends Component {
     render() {
         const bigLogo = {
-            fontSize: '400%'
+            fontSize: "400%"
         };
         return (
             <div className="row">
@@ -16,9 +16,9 @@ class HomePage extends Component {
                 </div>
                 <div className="col-md-4 col-md-offset-4">
                     <span>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
-                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-                        of type and scrambled it to make a type specimen book.
+                        O CODEX é um Objeto de Aprendizagem com a finalidade de auxiliar professores, estudantes e
+                        curiosos oferecendo um aplicativo gamificado que apoia o ensino e a aprendizagem de laços de
+                        repetição.
                     </span>
                 </div>
 
@@ -26,23 +26,23 @@ class HomePage extends Component {
                 <div className="navbar navbar-fixed-bottom">
                     <div className="col-lg-12">
                         <p>
-                            Criado por{' '}
+                            Criado por{" "}
                             <a href="http://thomaspark.co" rel="nofollow">
-                                Breno Teixeira e Bruno
+                                Breno Teixeira e Bruno Gonçalves
                             </a>.
                         </p>
                         <p>
-                            Código sobre a licença{' '}
+                            Código sobre a licença{" "}
                             <a href="https://github.com/thomaspark/bootswatch/blob/gh-pages/LICENSE">MIT License</a>.
                         </p>
                         <p>
-                            Baseado em{' '}
+                            Baseado em{" "}
                             <a href="http://getbootstrap.com" rel="nofollow">
                                 Bootstrap
-                            </a>. Icones de{' '}
+                            </a>. Icones de{" "}
                             <a href="http://fortawesome.github.io/Font-Awesome/" rel="nofollow">
                                 Font Awesome
-                            </a>. Web fonts de{' '}
+                            </a>. Web fonts de{" "}
                             <a href="http://www.google.com/webfonts" rel="nofollow">
                                 Google
                             </a>.
